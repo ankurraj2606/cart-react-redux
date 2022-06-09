a54431eeb748e01271e135b0e064480edad15192
ankurraj2606 <65525788+ankurraj2606@users.noreply.github.com>
2022-06-09 15:36:54 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice";
+import Modal from "./components/Modal";
+function App() {
+  const dispatch = useDispatch();
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems, dispatch]);
+
+  useEffect(() => dispatch(getCartItems()), [dispatch]);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading.....</h1>
+      </div>
+    );
+  }
+
+  return (
+    <main>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+export default App;
